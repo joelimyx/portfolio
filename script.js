8b9df77cd1b1609be15803cbce47a46f3c9761f4
Joe Lim <joelimyx@gmail.com>
2021-10-27 11:43:20 -0400

    js done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let attribute = document.getElementsByClassName("attribute");
+let attribute = document.getElementById("attribute");
+let show = document.getElementById("show");
+
+const showHide = () =>{
+    if(attribute.style.display == 'none'){
+        attribute.style.display = "block";
+    }else{
+        attribute.style.display = "none";
+    }
+}
+show.addEventListener("click",showHide);
